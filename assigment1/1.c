2469f3c8e7b9b140c0de207feba55ef6160940e4
Jay P Ermi <jayermi@silo.luddy.indiana.edu>
2023-01-13 15:01:42 -0500

    assigment1

--- assigment1/1.c
+++ assigment1/1.c
@@ -4,15 +4,15 @@
 //C program that uses puts to output text to screen
 
 int main() {
-
-   char father[255];
-   char son[255];
-   char result[255]; 
-   
-   strcpy(father, "ACM Code of Ethics and");
-   strcpy(son, "Proffesional conduct");
-   
-   puts(father);
-   puts(son);
-
+  
+   puts("A computing professional should..."); 
+   puts("Strive to achieve high quality in both the processes and products of professional work.");
+   puts("Maintain high standards of professional competence, conduct, and ethical practice.");
+   puts("Know and respect existing rules pertaining to professional work.");
+   puts("Accept and provide appropriate professional review.");
+   puts("Give comprehensive and thorough evaluations of computer systems and their impacts, including analysis of possible risks.");
+   puts("Perform work only in areas of competence.");
+   puts("Foster public awareness and understanding of computing, related technologies, and their consequences.");
+   puts("Access computing and communication resources only when authorized or when compelled by the public good.");
+   puts("Design and implement systems that are robustly and usably secure.");
 }
