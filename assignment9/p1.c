51c62a00ce1a3a35f5d962e58cb257338d3deb47
Jay P Ermi <jayermi@silo.luddy.indiana.edu>
2023-03-23 00:17:07 -0400

    completed assignment9

--- assignment9/p1.c
+++ assignment9/p1.c
@@ -1,47 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define SIZE 25 //macro for string size (unused) 
-char* mystrcat(char* dest, const char* src);
-char* mystrcat(char* dest, const char* src){
-   int T = 1;//True or False 1 is true 0 is false
-   int counter = 0;
-   while(T){
-      if(src[counter] == '\0'){//breakout of condition if a null terminaitor is reached
-         T = 0;
-      }
-      else{
-         dest[0] = src[counter];//populate the destiantion string with the source (incremented with counter)
-         counter++;//increase count and dest synonomously 
-         dest++;   
-      }
-   }
-   dest[0] = ' ';//handle spaces between words
-   dest++;
-   return dest;//return the new string 
-}
+void replacevalue(char **word, char **word2, char **word3);
+void send(char *word, char *word2, char *word3);
 
+void send(char *word, char *word2, char *word3){
+   replacevalue(&word, &word2, &word3);
+}
+void replacevalue(char **word, char **word2, char **word3){
+   char **temp = word;  // temp is a pointer to a *pointer*, not a pointer to a *char*
+    if(*temp != NULL) {
+        printf("%s ", *temp);
+        temp++;
+    }
+    char **temp2 = word2;
+    if(*temp2 != NULL) {
+        printf("%s ", *temp2);
+        temp2++;
+    }
+    char **temp3 = word3;
+    if(*temp3 != NULL) {
+        printf("%s", *temp3);
+        temp3++;
+    }
+}
 int main(){
-   char* str = (char*)calloc(100,1);//create space for our stings
-   char* str1 = (char*)calloc(100,1);
-   char* str2 = (char*)calloc(100,1); 
-   char combo[255];//make a character buffer to store our returned string 
-   char *p = combo;
-   char *p2 = p;
-   printf("Please enter your first word\n");//collect three string(s) 
-   scanf("%s", str);
-   printf("Please enter your second word\n");
-   scanf("%s", str1); 
-   printf("Please enter your third word\n");
-   scanf("%s", str2);
-   p2 = mystrcat(p2, str);
-   p2 = mystrcat(p2, str1);
-   p2 = mystrcat(p2, str2);
-   p2--;//
-   p2[0] = '\0';//implemented so program doesnt freak out from spcaes 
-   printf("%s", combo);
-   printf("\n");
-   free(str);
-   free(str1);
-   free(str2);
+   char* word;
+   word = malloc(4 * sizeof(word));
+   char* word2;
+   word2 = malloc(4 * sizeof(word2));
+   char* word3;
+   word3 = malloc(4 * sizeof(word3));
+   printf("Plese enter word 1:");
+   scanf("%s", word);
+   printf("Plese enter word 2:");
+   scanf("%s", word2);
+   printf("Plese enter word 3:");
+   scanf("%s", word3);
+   send(word, word2, word3);
    return 0;
 }
